f78fc48672a0252731eeb00b1c127955f06e3897
letcosta <40599622+letcosta@users.noreply.github.com>
2018-06-28 00:46:36 +0100

    Add files via upload

---
+++ Homework/Week1/Assign2_LG.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 27 08:55:02 2018
+
+@author: letcosta
+"""
+#%%
+""""
+Baby Names
+""""
+
+
+#%%
+""""
+tic-tac-toe
+""""
+board = [0,1,2,
+         3,4,5,
+         6,7,8]
